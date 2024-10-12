f3cd75a87f403e8ac4c9b003686cb2f363f5e49d
kination <inylove82@gmail.com>
2024-10-12 18:47:22 +0900

    Add cli command for read/write

--- depot-client/src/main.rs
+++ depot-client/src/main.rs
@@ -1,40 +1,81 @@
-
+use clap::{Parser, Subcommand};
 use s2n_quic::{client::Connect, Client};
 use std::{error::Error, path::Path};
 use std::net::{SocketAddr, ToSocketAddrs};
 
 use depot_common::Config;
 
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Write {
+        #[clap(long)]
+        host: Option<String>,
+
+        #[clap(long)]
+        port: Option<String>,
+        
+        #[clap(long)]
+        tls_cert_file_path: Option<String>,
+    },
+    Read {
+        #[clap(long)]
+        host: Option<String>,
+
+        #[clap(long)]
+        port: Option<String>,
+        
+        #[clap(long)]
+        tls_cert_file_path: Option<String>,
+    },
+}
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let config = Config::new();
-    let server_addr = format!("{}:{}", &config.server.host, &config.server.port).to_socket_addrs()?.next().unwrap();
-    let client = Client::builder()
-        .with_tls(Path::new(&config.server.tls.cert_file_path))?
-        .with_io("0.0.0.0:0")?
-        .start()?;
 
-    let addr: SocketAddr = server_addr;
-    let connect = Connect::new(addr).with_server_name("localhost");
-    let mut connection = client.connect(connect).await?;
+    let cli = Cli::parse();
 
-    // ensure the connection doesn't time out with inactivity
-    connection.keep_alive(true)?;
+    match cli.command {
+        Commands::Read { host, port, tls_cert_file_path } => {
 
-    // open a new bidirection stream
-    println!("--- Client started. Write down message and press enter ---");
-    let stream = connection.open_bidirectional_stream().await?;
-    let (mut receive_stream, mut send_stream) = stream.split();
+        }
+        Commands::Write { host, port, tls_cert_file_path } => {
+            let config = Config::new();
+            let server_addr = format!("{}:{}", &config.server.host, &config.server.port).to_socket_addrs()?.next().unwrap();
+            let client = Client::builder()
+                .with_tls(Path::new(&config.server.tls.cert_file_path))?
+                .with_io("0.0.0.0:0")?
+                .start()?;
 
-    // copy responses from the server
-    // tokio::spawn(async move {
-    //     let mut stdout = tokio::io::stdout();
-    //     let _ = tokio::io::copy(&mut receive_stream, &mut stdout).await;
-    // });
+            let addr: SocketAddr = server_addr;
+            let connect = Connect::new(addr).with_server_name("localhost");
+            let mut connection = client.connect(connect).await?;
 
-    // copy data from stdin and send it to the server
-    let mut stdin = tokio::io::stdin();
-    tokio::io::copy(&mut stdin, &mut send_stream).await?;
+            // ensure the connection doesn't time out with inactivity
+            connection.keep_alive(true)?;
+
+            // open a new bidirection stream
+            println!("--- Client started. Write down message and press enter ---");
+            let stream = connection.open_bidirectional_stream().await?;
+            let (mut receive_stream, mut send_stream) = stream.split();
+
+            // copy responses from the server
+            // tokio::spawn(async move {
+            //     let mut stdout = tokio::io::stdout();
+            //     let _ = tokio::io::copy(&mut receive_stream, &mut stdout).await;
+            // });
+
+            // copy data from stdin and send it to the server
+            let mut stdin = tokio::io::stdin();
+            tokio::io::copy(&mut stdin, &mut send_stream).await?;
+        }
+    }
 
     Ok(())
 }
