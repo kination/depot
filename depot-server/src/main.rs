d5aa7a3082308557caef01b6de18b9bdd8ebacbe
kination <inylove82@gmail.com>
2024-11-16 22:29:03 +0900

    Use new-defined config for server

--- depot-server/src/main.rs
+++ depot-server/src/main.rs
@@ -1,68 +1,109 @@
+use rustls::server;
 use s2n_quic::stream::BidirectionalStream;
 use s2n_quic::Server;
 use std::net::ToSocketAddrs;
 use std::sync::Arc;
 use std::{error::Error, path::Path};
+use serde::{Serialize, Deserialize};
 
-use depot_common::Config;
+// use depot_common::Config;
 use depot_common::MessageQueue;
-use tokio::sync::{Mutex, Notify};
+use tokio::sync::Mutex;
+
+
+#[derive(Serialize, Deserialize)]
+struct ServerConfig {
+    inputs: Vec<InputConfig>,
+    setting: Settings,
+}
+
+#[derive(Serialize, Deserialize)]
+struct InputConfig {
+    tag: String,
+    parse: Option<ParseConfig>,
+    filter: Option<FilterConfig>, // Optional since not all inputs have a filter
+    produce: Vec<ProduceConfig>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ParseConfig {
+    r#type: String,
+    exp: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct FilterConfig {
+    rule: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ProduceConfig {
+    r#type: String,
+    host: Option<String>,
+    port: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Settings {
+    host: String,
+    port: String,
+    tls_cert_file: String,
+    tls_key_file: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct MessageFormat {
+    timestamp: String,
+    tag: String,
+    message: String,
+}
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let config = Config::new();
-    let server_addr = format!("{}:{}", &config.server.host, &config.server.port)
+    let server_config: ServerConfig = serde_yaml::from_reader(
+        std::fs::File::open(
+            "/Users/kination/workspace/public/depot/configs/sample-server-config.yaml"
+        )?
+    )?;
+    let server_settings = server_config.setting;
+
+    let server_addr = format!("{}:{}", &server_settings.host, &server_settings.port)
         .to_socket_addrs()?
         .next()
         .unwrap();
     let mut server = Server::builder()
         .with_tls((
-            Path::new(&config.server.tls.cert_file_path),
-            Path::new(&config.server.tls.key_file_path),
+            Path::new(&server_settings.tls_cert_file),
+            Path::new(&server_settings.tls_key_file),
         ))?
         .with_io(server_addr)?
         .start()?;
-    let queue = Arc::new(Mutex::new(MessageQueue::new()));
+    // let queue = Arc::new(Mutex::new(MessageQueue::new()));
 
     println!("--- Server started in {} ---", server_addr.to_string());
     while let Some(mut connection) = server.accept().await {
-        let queue = Arc::clone(&queue);
+        // let queue = Arc::clone(&queue);
 
         tokio::spawn(async move {
             while let Ok(Some(mut stream)) = connection.accept_bidirectional_stream().await {
                 println!("new connection!!");
-                let queue = Arc::clone(&queue);
+                // let queue = Arc::clone(&queue);
 
                 tokio::spawn(async move {
-                    // First, read the type of connection (writer or reader)
-                    if let Ok(Some(connection_type)) = stream.receive().await {
-                        match String::from_utf8_lossy(&connection_type.to_vec()).as_ref() {
-                            "writer" => {
-                                println!("Start writer");
-                                // Handle writer connection
-                                while let Ok(Some(data)) = stream.receive().await {
-                                    println!("Received data from writer: {:?}", data);
-                                    let queue_guard = queue.lock().await;
-                                    queue_guard.push(data.clone()).await;
-                                    println!("Queue contents: {:?}", *queue_guard);
-                                }
-                            }
-                            "reader" => {
-                                println!("Start reader");
-                                loop {
-                                    let queue_guard = queue.lock().await;
-                                    if let Some(data) = queue_guard.pop().await {
-                                        println!("Sending data to reader: {:?}", data);
-                                        if let Err(e) = stream.send(data).await {
-                                            eprintln!("Failed to send data to reader: {:?}", e);
-                                        }
-                                    }
-                                }
-                            }
-                            _ => {
-                                eprintln!("Unknown connection type: {:?}", connection_type);
-                            }
+                    while let Ok(Some(data)) = stream.receive().await {
+                        println!("Received data: {:?}", data);
+                        if data.is_empty() {
+                            println!("No data");
+                            continue;
                         }
+                        let message: MessageFormat = serde_json::from_slice(&data).unwrap();
+                        // println!("Deserialized message: {:?}", message);
+
+                        // let queue_guard = queue.lock().await;
+                        // queue_guard.push(data.clone()).await;
+                        // println!("Queue contents: {:?}", *queue_guard);
                     }
                 });
             }
@@ -70,34 +111,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-// async fn save_queue_to_file(queue: &MessageQueue) -> std::io::Result<()> {
-//     let file = tokio::fs::File::create("sample_queue.txt").await?;
-//     let mut writer = tokio::io::BufWriter::new(file);
-//     // for item in queue {
-//     //     writer.write_all(item.as_bytes()).await?;
-//     //     writer.write_all(b"\n").await?;
-//     // }
-
-//     Ok(())
-// }
-
-async fn handle_read_request(queue: Arc<Mutex<MessageQueue>>, stream: &mut BidirectionalStream) {
-    let notify = Arc::new(Notify::new());
-    let queue_guard = queue.lock().await;
-    while let Some(data) = queue_guard.pop().await {
-        // Assuming pop() returns Option<DataType>
-        // Send the data back to the client
-        if let Err(e) = stream.send(data).await {
-            eprintln!("Failed to send data to reader: {:?}", e);
-        }
-    }
-
-    notify.notify_one();
-    // } else {
-    //     // Optionally send a message indicating the queue is empty
-    //     let empty_message = "Queue is empty".to_string();
-    //     let _ = stream.send(empty_message.into()).await;
-    // }
 }
