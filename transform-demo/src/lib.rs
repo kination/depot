446f269d3764a1cdd3069cfeeec8793c2c3b744e
kination <inylove82@gmail.com>
2024-11-28 00:55:46 +0900

    Apply regex option for demo transformer

--- transform-demo/src/lib.rs
+++ transform-demo/src/lib.rs
@@ -1,22 +1,15 @@
-// use regex::Regex;
-
-// Define a trait for transformation
-// pub trait Transformer {
-//     fn transform(&self, message: &str) -> String;
-// }
 
 // Implement the trait for each transformation module
 pub mod transform {
-    // use super::Transformer;
     use depot_common::Transformer;
+    use std::collections::HashMap;
 
     pub struct TransformDemoModule;
 
     impl Transformer for TransformDemoModule {
-        fn transform(&self, message: &str) -> String {
-            // Your transformation logic here
-            message.to_uppercase() // Example transformation
+        fn transform(&self, message: &str, option: Option<HashMap<String, String>>) -> Result<String, String> {
+            // TODO: Your transformation logic here
+            Ok(message.to_lowercase())
         }
     }
 }
-
---
+++ transform-regex/src/lib.rs
@@ -0,0 +1,39 @@
+pub mod transform {
+    use std::collections::HashMap;
+    use regex::Regex;
+    use depot_common::Transformer;
+
+    pub struct TransformRegexModule;
+
+    impl Transformer for TransformRegexModule {
+        fn transform(&self, message: &str, option: Option<HashMap<String, String>>) -> Result<String, String> {
+            if let Some(ref opts) = option {
+                let expression = opts.get("exp").ok_or("Error: 'exp' key not found!")?;
+                let regex = Regex::new(expression).unwrap();
+
+                if let Some(caps) = regex.captures(message) {
+                    let mut json_data = serde_json::json!({});
+
+                    if let Some(ref opts) = option {
+                        // Iterate over the keys in the options to build the JSON object
+                        for (key, capture_name) in opts.iter() {
+                            if let Some(capture) = caps.name(capture_name) {
+                                json_data[key] = serde_json::json!(capture.as_str());
+                            } else {
+                                json_data[key] = serde_json::json!("");
+                            }
+                        }
+                    }
+                    return Ok(json_data.to_string())
+                } else {
+                    // println!("No matches found.");
+                    // assert!(false, "Expected captures but found none.");
+                    // return Err("No matches found.".to_string());
+                }
+            }
+
+            Err(message.to_uppercase().to_string())
+        }
+    }
+}
+
